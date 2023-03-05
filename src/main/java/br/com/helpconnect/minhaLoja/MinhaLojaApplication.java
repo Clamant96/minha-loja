18d0dc0d55a5097e6570837918676772b207d5e0
Kevin Alec Neri Lazzarotto <kevin.lazzarotto@hotmail.com>
2023-03-05 20:16:09 -0300

    Criacao de projeto

---
+++ src/main/java/br/com/helpconnect/minhaLoja/MinhaLojaApplication.java
@@ -0,0 +1,14 @@
+package br.com.helpconnect.minhaLoja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaLojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaLojaApplication.class, args);
+	}
+	
+
+}
